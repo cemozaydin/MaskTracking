c762739bcffcb3e95586d05681f1fd80166ae881
Cem Özaydın <cem.ozaydin@gmail.com>
2024-02-12 14:20:50 +0300

    second commit

--- Business/obj/Debug/net6.0/Business.AssemblyInfo.cs
+++ Business/obj/Debug/net6.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a59f49a207e4c99b60ccff575ef24f06d333db3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
